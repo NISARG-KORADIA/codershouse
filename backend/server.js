52a8e9fa9c7d4bfd96cb35ae2a99cc309e507b05
NISARG-KORADIA <mknisarg@gmail.com>
2022-03-12 12:12:32 +0530

    Working Prototype of WebRTC drop-in audio chat

--- backend/server.js
+++ backend/server.js
@@ -117,6 +117,19 @@ io.on('connection', (socket) => {
     });
   });
 
+  socket.on(ACTIONS.MUTE_INFO, ({ userId, roomId, isMute }) => {
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+    clients.forEach((clientId) => {
+      if (clientId !== socket.id) {
+        console.log('mute info');
+        io.to(clientId).emit(ACTIONS.MUTE_INFO, {
+          userId,
+          isMute,
+        });
+      }
+    });
+  });
+
   const leaveRoom = () => {
     const { rooms } = socket;
     // console.log('leaving', rooms);
